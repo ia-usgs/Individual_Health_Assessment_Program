f7b6e6c8273b733cd9f64838b92189c9312393bd
ia-usgs <irvin.i.avitia@gmail.com>
2023-07-03 15:40:08 -0700

    forgot to square the formula

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class Main {
+public class BMICalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter weight (in kg)- ");
