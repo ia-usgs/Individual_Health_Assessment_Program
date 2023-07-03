57231c92364e71e4cc10481bf858671bf2c9d84b
ia-usgs <irvin.i.avitia@gmail.com>
2023-07-03 15:34:52 -0700

    Fixed error where I couldn't input decimals

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,15 @@ public class Main {
 
         double bmi =  weight / height;
         System.out.println("Your BMI is: " + bmi);
-        if (bmi < 18) {
-            System.out.println("You are seriously underweight!");
-        } else if (bmi < 18.5) {
-            System.out.println("You are underweight!");
-        } else if (bmi < 25) {
-            System.out.println("You are normal weight!");
-        } else if (bmi < 30) {
-            System.out.println("You are obese!");
-        }
+            if (bmi < 18) {
+                System.out.println("You are seriously underweight!");
+            } else if (bmi < 18.5) {
+                System.out.println("You are underweight!");
+            } else if (bmi < 25) {
+                System.out.println("You are normal weight!");
+            } else if (bmi < 30) {
+                System.out.println("You are obese!");
+            }
 
 
     }
