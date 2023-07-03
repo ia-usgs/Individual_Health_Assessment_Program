73ffcffc936219458d445711b548c8f0c94de917
ia-usgs <irvin.i.avitia@gmail.com>
2023-07-03 15:38:43 -0700

    forgot to square the formula

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,17 @@ public class Main {
         System.out.println("Enter Height (in meters)- ");
         double height = sc.nextDouble();
 
-        double bmi =  weight / height;
+        double bmi =  weight / (height * height);
         System.out.println("Your BMI is: " + bmi);
-            if (bmi < 18) {
-                System.out.println("You are seriously underweight!");
-            } else if (bmi < 18.5) {
-                System.out.println("You are underweight!");
-            } else if (bmi < 25) {
-                System.out.println("You are normal weight!");
-            } else if (bmi < 30) {
-                System.out.println("You are obese!");
-            }
+        if (bmi < 18) {
+            System.out.println("You are seriously underweight!");
+        } else if (bmi < 18.5) {
+            System.out.println("You are underweight!");
+        } else if (bmi < 25) {
+            System.out.println("You are normal weight!");
+        } else {
+            System.out.println("You are obese!");
+        }
 
 
     }
